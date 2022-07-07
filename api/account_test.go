598b9c6e097e80d1932ceac1803cda108e5a0180
islamghany <islamghany3@gmail.com>
2022-07-07 12:06:28 +0200

    added TDD

--- api/account_test.go
+++ api/account_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"database/sql"
 	"fmt"
 	mockdb "github/islmaghany/bank/db/mock"
 	db "github/islmaghany/bank/db/sqlc"
@@ -8,41 +9,95 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 )
 
 func TestGetAccount(t *testing.T) {
-	account := createAccount()
+	testCases := []struct {
+		name         string
+		accountID    int64
+		buildStubs   func(store *mockdb.MockStore)
+		expectStatus int
+	}{
+		{
+			name:      "OK",
+			accountID: 1,
+			buildStubs: func(store *mockdb.MockStore) {
+				const id int64 = 1
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(id)).
+					Return(randomAccount(id), nil).
+					Times(1)
+			},
+			expectStatus: http.StatusOK,
+		},
+		{
+			name:      "NotFound",
+			accountID: 2,
+			buildStubs: func(store *mockdb.MockStore) {
+				const id int64 = 2
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(id)).
+					Return(db.Account{}, sql.ErrNoRows).
+					Times(1)
+			},
+			expectStatus: http.StatusNotFound,
+		},
+		{
+			name:      "InternalError",
+			accountID: 3,
+			buildStubs: func(store *mockdb.MockStore) {
+				const id int64 = 3
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Eq(id)).
+					Return(db.Account{}, sql.ErrConnDone).
+					Times(1)
+			},
+			expectStatus: http.StatusInternalServerError,
+		},
+		{
+			name:      "BadRequest",
+			accountID: 0,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetAccount(gomock.Any(), gomock.Any()).
+					Times(0)
+			},
+			expectStatus: http.StatusBadRequest,
+		},
+	}
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-	store := mockdb.NewMockStore(ctrl)
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
 
-	store.EXPECT().GetAccount(gomock.Any(), gomock.Eq(account.ID)).Times(1)
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
 
-	server := NewServer(store)
+			url := fmt.Sprintf("/accounts/%d", tc.accountID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
 
-	recorder := httptest.NewRecorder()
-	url := fmt.Sprintf("/accounts/%d", account.ID)
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-
-	require.NoError(t, err)
-
-	server.router.ServeHTTP(recorder, req)
-
-	require.Equal(t, http.StatusOK, recorder.Code)
-	//require.
-
+			server.router.ServeHTTP(recorder, request)
+			require.Equal(t, tc.expectStatus, recorder.Code)
+		})
+	}
 }
-func createAccount() *db.Account {
-	return &db.Account{
-		ID:       util.RandomInt(1, 100),
-		Owner:    util.RandomOwner(),
-		Balance:  util.RandomMoney(),
-		Currency: util.RandomCurrency(),
+
+func randomAccount(id int64) db.Account {
+	return db.Account{
+		ID:        id,
+		Owner:     util.RandomOwner(),
+		Balance:   util.RandomMoney(),
+		Currency:  util.RandomCurrency(),
+		CreatedAt: time.Now(),
 	}
 }
